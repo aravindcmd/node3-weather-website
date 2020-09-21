caccfe655e9d53add31e6b01d27d66b7082bb94f
aravindcmd <aravindsrisai100@gmail.com>
2020-09-21 14:58:00 +0530

    removed unnecessary console.log statements in app file

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-console.log('loaded')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=loba').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-
-// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
